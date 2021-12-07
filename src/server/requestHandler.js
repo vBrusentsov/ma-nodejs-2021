806076d8c47b53fe4c1d59269ac5f73e08c7af75
Vlad Brusentsov <Arconiel456@gmail.com>
2021-12-07 21:55:02 +0200

    add logic to capture csv file

--- src/server/requestHandler.js
+++ src/server/requestHandler.js
@@ -19,8 +19,14 @@ module.exports = (req, res) => {
     })
     .on('end', () => {
       const stringBody = Buffer.concat(body).toString();
+      if (req.headers['content-type'] === 'text/csv') {
+        return routes(
+          { ...req, pathname, body: stringBody, params: searchParams },
+          res,
+        );
+      }
       body = stringBody ? JSON.parse(stringBody) : undefined;
       res.setHeader('Content-Type', 'application/json');
-      routes({ ...req, pathname, body, params: searchParams }, res);
+      return routes({ ...req, pathname, body, params: searchParams }, res);
     });
 };
